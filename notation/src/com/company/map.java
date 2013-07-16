e4f516b546a522f3956e09c1ae4e5e24ca9a4acc
Artem Kuraev <artem.kuraev@gmail.com>
2013-07-16 20:13:59 +0400

    initial

---
+++ notation/src/com/company/map.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: art
+ * Date: 16.07.13
+ * Time: 20:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class map {
+}
