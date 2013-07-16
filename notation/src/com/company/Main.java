8d9c183309285caeaa6be6eef5abd8f500e83afe
Artem Kuraev <artem.kuraev@gmail.com>
2013-07-16 23:38:20 +0400

    Изменил алгоритм на верный, всё считает правильно, класс маин сделал похожим на ООП, но, по хорошему, надо оттуда всё выносить в отдельный класс Solver.

--- notation/src/com/company/Main.java
+++ notation/src/com/company/Main.java
@@ -3,11 +3,29 @@ package com.company;
 import java.util.Stack;
 
 public class Main {
+    //По хорошему, в данный метод надо бы добавить исключение FormatErrorException
+    private static Integer solve(int operand2, String operation, int operand1) {
+        if (operation.equals("*")) {
+            return(operand1 * operand2);
+        }
+        if (operation.equals("/")) {
+            return(operand1 / operand2);
+        }
+        if (operation.equals("+")) {
+            return(operand1 + operand2);
+        }
+        if (operation.equals("-")) {
+            return(operand1 - operand2);
+        }
+        return 0;
+    }
+    private static final Stack<Object> stack = new Stack<Object>();
+    private static final dictionary dict = new dictionary();
+
 
     public static void main(String[] args) {
-        Stack<Object> stack = new Stack<Object>();
-        dictionary dict = new dictionary();
         //IX * VII - XX + XVII / II - XI
+        //9 * 7 - 20 + 17 / 2 - 11
         /*1-I 2-II 3-III 4-IV 5-V 6-VI 7-VII 8-VIII 9-IX 10-X
         11-XI 12-XII 13-XIII 14-XIV 15-XV 16-XVI 17-XVII 18-XVIII
         19-XIX 20-XX*/
@@ -15,7 +33,7 @@ public class Main {
         dict.put(new String("IX"), 9);
         dict.put(new String("VII"), 7);
         dict.put(new String("XX"), 20);
-        dict.put(new String("XVII"), 13);
+        dict.put(new String("XVII"), 17);
         dict.put(new String("II"), 2);
         dict.put(new String("XI"), 11);
         int result;
@@ -25,15 +43,7 @@ public class Main {
             } else {
                 if (arg.equals("-") || arg.equals("+")) {
                     while (stack.size() > 1) {
-                        Integer op1 = (Integer)stack.pop();
-                        String operator = (String)stack.pop();
-                        Integer op2 = (Integer)stack.pop();
-                        if (operator.equals("*")) {
-                            stack.push(op1 * op2);
-                        }
-                        if (operator.equals("/")) {
-                            stack.push(op1 / op2);
-                        }
+                        stack.push(solve((Integer)stack.pop(),(String)stack.pop(), (Integer)stack.pop()));
                     }
                     stack.push(arg);
                 } else {
@@ -42,15 +52,7 @@ public class Main {
             }
         }
         while (stack.size() > 1) {
-            Integer op1 = (Integer)stack.pop();
-            String operator = (String)stack.pop();
-            Integer op2 = (Integer)stack.pop();
-            if (operator.equals("+")) {
-                stack.push(op1 + op2);
-            }
-            if (operator.equals("-")) {
-                stack.push(op1 - op2);
-            }
+            stack.push(solve((Integer)stack.pop(),(String)stack.pop(), (Integer)stack.pop()));
         }
         System.out.println(stack.pop());
 	// write your code here
